1317d417fd0ad263c88a00f969707cd60773d3b3
marcus62901646 <marcusmom09@gmail.com>
2026-08-12 17:19:05 -0300

    start of menu work.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace CakeMayhem
+{
+	public class MenuManager : MonoBehaviour
+	{
+		void Start() { }
+		void PlayScene() { }
+		void Language() { }
+		void VolumeManage() { }
+
+	}
+}

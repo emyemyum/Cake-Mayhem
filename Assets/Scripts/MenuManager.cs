5658814169f38b767be4dbbfeb19701845127e38
marcus62901646 <marcusmom09@gmail.com>
2026-09-08 17:49:07 -0300

    new cake menu button sprites, start on menu manager script.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace cake_mayhem
+{
+    public class MenuManager
+    {
+        void PlayButton() { }
+        void VolSlider() { }
+        void MusSlider() { }
+        void QuitButton() { }
+    }
+}
